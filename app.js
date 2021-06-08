5c38af4df61184248011d968242da708d50ff16e
Tristan Angieri <tristanangieri@Tristans-MacBook-Pro.local>
2021-06-08 14:49:14 -0400

    Setup bookmarksController route

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 // DEPENDENCIES
 const express = require("express");
 
+
 // CONFIGURATION
 const app = express();
+const bookmarksController = require("./controllers/bookmarksController.js");
 
 // ROUTES
 app.get("/", (req, res) => {
     res.send("Welcome to the Bookmarks App!");
+});
+
+app.use("/bookmarks", bookmarksController);
+
+app.get("*", (req, res) => {
+    res.status(404).send("Page not found.");
 });
 
 // EXPORT
