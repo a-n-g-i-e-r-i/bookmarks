9ed9046e0e5f13625aef19b11d8142a25d547b59
Tristan Angieri <tristanangieri@Tristans-MacBook-Pro.local>
2021-06-09 13:11:11 -0400

    added show and create routes to bookmarksController

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ const app = express();
 const bookmarksController = require("./controllers/bookmarksController.js");
 
 // ROUTES
+app.use((req, res, next) => {
+    console.log(`[development] Middleware is running!`);
+    next();
+});
+
+app.use(express.json()); // Parse all incoming requests as JSON
+
 app.get("/", (req, res) => {
     res.send("Welcome to the Bookmarks App!");
 });
--- controllers/bookmarksController.js
+++ controllers/bookmarksController.js
@@ -2,8 +2,44 @@ const express = require("express");
 const bookmarks = express.Router();
 const bookmarksArray = require("../models/bookmark.js");
 
+const validateUrl = (req, res, next) => {
+    const http = "http://";
+    const https = "https://";
+    var fullUrl = req.protocol + '://' + req.get('host') + req.url;
+    console.log(`[development] Request URL: ${fullUrl}`);
+    if (
+        fullUrl.substring(0, 7) === http ||
+        fullUrl.substring(0, 8) === https
+    ) {
+        return next();
+    } else {
+        res
+        .status(400)
+        .send(`Oops, you forgot to start your url with http:// or https://`);
+    }
+    };
+
+bookmarks.use(validateUrl);
+
 bookmarks.get("/", (req, res) => {
     res.status(200).json(bookmarksArray);
 });
+
+bookmarks.get("/:arrayIndex", (req, res) => {
+    const { arrayIndex } = req.params;
+    if(bookmarksArray[arrayIndex]) {
+        res.status(200).json(bookmarksArray[arrayIndex - 1]);
+    } else {
+        res.redirect("/404");
+    };
+});
+
+bookmarks.post("/", (req, res) => {
+    // Create the new resource
+    bookmarksArray.push(req.body);
+    // Send back the new resource as confirmation
+    res.json(bookmarksArray[bookmarksArray.length - 1]);
+});
+
 
 module.exports = bookmarks;
